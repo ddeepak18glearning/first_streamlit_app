9f4c11b51e9f59b2731f8560f0f4b6054a07bea5
DEEPAK GUPTA <110766587+ddeepak18glearning@users.noreply.github.com>
2022-08-14 22:24:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,23 +26,23 @@ streamlit.dataframe(fruits_to_show)
 
 
 #create function
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+#def get_fruityvice_data(this_fruit_choice):
+#    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+#    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#    return fruityvice_normalized
    
 #new section to display Fruityvice API response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-            streamlit.error("Please select a fruit for information.")
-   else:
-         back_from_function = get_fruityvice_data(fruit_choice)
-         streamlit.dataframe(back_from_function)
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#            streamlit.error("Please select a fruit for information.")
+#   else:
+#         back_from_function = get_fruityvice_data(fruit_choice)
+#         streamlit.dataframe(back_from_function)
 
-except URLError as e:
-    streamlit.error()
+#except URLError as e:
+#    streamlit.error()
 
 streamlit.header("The Fruit Load List Contains:")
 #snowflake related functions
